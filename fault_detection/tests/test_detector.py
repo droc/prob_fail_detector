5646b77d13470987bfad43fda4427324e4d2ea1d
droc <jigermano@gmail.com>
2012-06-17 21:51:51 -0300

    Refactoring of test code: clearer setup

--- fault_detection/tests/test_detector.py
+++ fault_detection/tests/test_detector.py
@@ -58,21 +58,24 @@ class TestDetector(unittest.TestCase):
         self.detector = BinomialDetector(10, 0.1)
 
     def test_given_a_history_of_test_results_without_noise_where_the_test_began_to_fail_detects_fails(self):
-        test_results = a_test_result_history().with_number_of_results(20).with_p_of_fail(1, since=10).build()
+        test_results = self.results().with_p_of_fail(1, since=10).build()
         self.assertTrue(self.detector.detects_fail(test_results))
 
     def test_given_a_history_of_test_results_with_all_fails_doesnt_detect_breakage(self):
-        test_results = a_test_result_history().with_number_of_results(20).with_all_fails().build()
+        test_results = self.results().with_all_fails().build()
         self.assertFalse(self.detector.detects_fail(test_results))
 
     def test_given_a_history_of_test_results_without_noise_with_all_passing_doesnt_detect_approx_breakage_date(self):
-        test_results = a_test_result_history().with_number_of_results(20).all_passing().build()
+        test_results = self.results().all_passing().build()
         self.assertFalse(self.detector.detects_fail(test_results))
 
+    def results(self):
+        return a_test_result_history().with_number_of_results(20)
+
     def test_given_a_history_of_test_results_with_noise_where_the_test_began_to_fail_constantly_detects_fails(self):
-        test_results = a_test_result_history().with_number_of_results(20).with_p_of_fail(1, since=10).with_noise().build()
+        test_results = self.results().with_p_of_fail(1, since=10).with_noise().build()
         self.assertTrue(self.detector.detects_fail(test_results))
 
     def test_given_a_history_of_test_results_with_noise_where_the_test_dont_began_to_fail_doesnt_detect_fails(self):
-        test_results = a_test_result_history().with_number_of_results(20).with_noise().build()
+        test_results = self.results().with_noise().build()
         self.assertFalse(self.detector.detects_fail(test_results))
