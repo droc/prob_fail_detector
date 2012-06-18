5b265cc5f4dbb9e0a23a9df28ec8ce01b489fb7b
droc <jigermano@gmail.com>
2012-06-17 22:02:12 -0300

    Added setup.py and added gmpy as requirement

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fault_detection',
+    version='0.1',
+    packages=['fault_detection'],
+    url='',
+    license='',
+    author='Juan',
+    author_email='',
+    description='', requires=['gmpy']
+)
